4e02ecb7333b6369375f91964efd876c1d60351c
Adam Araújo <adamcsaraujo@gmail.com>
2020-02-27 14:51:53 -0300

    Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import logo from './assets/logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <img src={logo} alt="SGRHU"/>
+
+      <div className="content">
+        <h1>
+          SEJA BEM-VINDO (A)!
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Para ter acesso ao sistema, entre com suas credenciais abaixo
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <form>
+          <input 
+            type="email" 
+            id="email" 
+            placeholder="Digite o seu login"
+          />
+          <input 
+            type="password" 
+            id="password" 
+            placeholder="Digite a sua senha"
+          />
+
+          <button className="btn" type="submit">ENTRAR</button>
+        </form>
+      </div>
     </div>
   );
 }
