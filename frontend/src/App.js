04bfb6f07eb142c65595666fa029bcdb190a8cc0
Adam Araújo <adamcsaraujo@gmail.com>
2020-02-27 21:20:05 -0300

    Login finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 
 import logo from './assets/logo.png';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faSignInAlt, faLock } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUser, faSignInAlt, faLock)
+
 function App() {
   return (
     <div className="container">
@@ -10,25 +16,33 @@ function App() {
 
       <div className="content">
         <h1>
-          SEJA BEM-VINDO (A)!
+          Seja bem-vindo!
         </h1>
         <p>
           Para ter acesso ao sistema, entre com suas credenciais abaixo
         </p>
 
         <form>
-          <input 
-            type="email" 
-            id="email" 
-            placeholder="Digite o seu login"
-          />
-          <input 
-            type="password" 
-            id="password" 
-            placeholder="Digite a sua senha"
-          />
+          <div className="input-icon">
+            <div className="icon"><FontAwesomeIcon icon="user" /></div>
+            <input 
+              type="email" 
+              id="email" 
+              placeholder="Digite o seu login"
+            />
+          </div>
+          <div className="input-icon">
+            <div className="icon"><FontAwesomeIcon icon="lock" /></div>
+            <input 
+              type="password" 
+              id="password" 
+              placeholder="Digite a sua senha"
+            />
+          </div>
 
-          <button className="btn" type="submit">ENTRAR</button>
+          <button className="btn" type="submit">
+            <FontAwesomeIcon icon="sign-in-alt" />  ENTRAR
+          </button>
         </form>
       </div>
     </div>
