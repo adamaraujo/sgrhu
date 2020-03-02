974db36a6e3e51c6e2e2ca4b3d85fd5af27cf201
Adam Araújo <adamcsaraujo@gmail.com>
2020-03-01 22:20:58 -0300

    Login v2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 
+import styled from 'styled-components';
+import {User, LockAlt} from 'styled-icons/boxicons-solid';
+import {SignInAlt} from 'styled-icons/fa-solid/SignInAlt';
+
 import logo from './assets/logo.png';
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faSignInAlt, faLock } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faUser, faSignInAlt, faLock)
 
 function App() {
   return (
@@ -16,32 +14,32 @@ function App() {
 
       <div className="content">
         <h1>
-          Seja bem-vindo!
+          Olá, bem-vindo de volta!
         </h1>
         <p>
-          Para ter acesso ao sistema, entre com suas credenciais abaixo
+          Para ter acesso ao sistema, entre com suas <strong>credenciais</strong> abaixo
         </p>
 
         <form>
           <div className="input-icon">
-            <div className="icon"><FontAwesomeIcon icon="user" /></div>
             <input 
               type="email" 
               id="email" 
               placeholder="Digite o seu login"
             />
+            <i><User size="18"/></i>
           </div>
           <div className="input-icon">
-            <div className="icon"><FontAwesomeIcon icon="lock" /></div>
             <input 
               type="password" 
               id="password" 
               placeholder="Digite a sua senha"
             />
+            <i><LockAlt size="18"/></i>
           </div>
 
           <button className="btn" type="submit">
-            <FontAwesomeIcon icon="sign-in-alt" />  ENTRAR
+            <SignInAlt size="18"/>  ENTRAR
           </button>
         </form>
       </div>
