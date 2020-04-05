bcaf86628993783c2a402cd2438ab782a109b97d
Adam Araújo <adamcsaraujo@gmail.com>
2020-04-05 12:10:31 -0300

    Request Acompanhante OK

--- backend/src/main/java/com/backend/backend/controllers/TicketController.java
+++ backend/src/main/java/com/backend/backend/controllers/TicketController.java
@@ -37,6 +37,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.backend.repository.DAOAtendente;
+import com.backend.backend.repository.DAOAutorizacao;
 import com.backend.backend.repository.DAOChefia;
 import com.backend.backend.repository.DAORefeicao;
 import com.backend.backend.repository.DAOTicket;
@@ -58,16 +59,16 @@ public class TicketController {
 	private DAOTicketAvulso daoTicketAvulso;
 	private DAOTicketNormal daoTicketNormal;
 	private DAORefeicao daoRefeicao;
-	//private consultaHospital consultaHU;
+	private DAOAutorizacao daoAutorizacao;
 	
 	@Autowired
-	TicketController (DAOChefia chefiaRepository, DAOAtendente atendenteRepository, DAOTicket ticketRepository, DAOTicketAvulso avulsoRepository, DAOTicketNormal normalRepository, DAORefeicao refeicaoRepository) {
+	TicketController (DAOChefia chefiaRepository, DAOAtendente atendenteRepository, DAOTicket ticketRepository, DAOTicketAvulso avulsoRepository, DAOTicketNormal normalRepository, DAORefeicao refeicaoRepository, DAOAutorizacao autorizacaoRepository) {
 		this.daoTicket = ticketRepository;
 		this.daoTicketAvulso = avulsoRepository;
 		this.daoTicketNormal = normalRepository;
 		this.daoChefia = chefiaRepository;
 		this.daoAtendente = atendenteRepository;
-		//this.consultaHU = huRepository;
+		this.daoAutorizacao = autorizacaoRepository;
 	}
 	
 	public Connection connect() throws SQLException {
@@ -81,40 +82,7 @@ public class TicketController {
 	      return rs;
 	}
 	
-	@RequestMapping(value = "/consulta/paciente/{cpf}", method = RequestMethod.GET, produces="application/json") 
-	public ResponseEntity<String> consultaPaciente(@PathVariable String cpf) {
-		JSONArray entities = new JSONArray();
-		String buscaPaciente = "SELECT * FROM essgrhu.paciente AS paciente "
-        		+ "NATURAL JOIN essgrhu.internacao AS internacao "
-        		+ "NATURAL JOIN essgrhu.leito  AS leito "
-        		+ "NATURAL JOIN essgrhu.unidadeFuncional AS unidadeF "
-        		+ "WHERE paciente.cpfpaciente = '" + cpf + "';";
-		
-		try {
-			ResultSet ps = retrieveData(buscaPaciente);
-				while(ps.next()) {
-					JSONObject record = new JSONObject();
-				    record.put("cpfpaciente", ps.getString("cpfpaciente"));
-				    record.put("primeiro_nome", ps.getString("primeiro_nome"));
-				    record.put("sobrenome", ps.getString("sobrenome"));
-				    record.put("datainternacao", ps.getDate("datainternacao"));
-				    record.put("dataprevisao", ps.getDate("dataprevisao"));
-				    record.put("horainternacao", ps.getTime("horainternacao"));
-				    record.put("horaprevisao", ps.getTime("horaprevisao"));
-				    record.put("tipo", ps.getString("tipo"));
-				    entities.put(record);
-				}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-			String result = entities.toString();
-			if (result.length() != 2) {
-				return new ResponseEntity<String>(result, HttpStatus.OK);
-			}
-			else {
-				return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
-			}
-	}
+	/*
 	
 	@RequestMapping(value = "/consulta/colaborador/{cpf}", method = RequestMethod.GET, produces="application/json") 
 	public ResponseEntity<String> consultaColaborador(@PathVariable String cpf) {
@@ -153,59 +121,19 @@ public class TicketController {
 			}
 	}
 	
+	*/
+	
 	@GetMapping // Não precisa indicar o caminho, pois para este caso, será sempre /tickets
 	public List listar(){
 	   return daoTicket.findAll();
 	}
 	
-	// && consumidoHOJE.getNomeRefeicao() == ticket.getNomeRefeicao()
-	
-	// Criando novo ticket
-	/*
-	@RequestMapping(value = "/normal/new", method = RequestMethod.POST)
-	public void criar(@RequestBody Ticket ticket) {
-		List<Ticket> consumidoHOJE = daoTicket.findBycpfCliente(ticket.getCpfCliente());
-		
-		int i = 0;
-		
-		String dataTicket = ticket.getDataConsumo();
-		String nomeTicket = ticket.getNomeRefeicao();
-		
-		while (i < consumidoHOJE.size()) {
-			String dataConsumido = consumidoHOJE.get(i).getDataConsumo();
-			String nomeConsumido = consumidoHOJE.get(i).getNomeRefeicao();
-			if (dataTicket.equals(dataConsumido) && nomeTicket.equals(nomeConsumido)) {
-				//return ResponseEntity.notFound().build();
-			}
-			i = i + 1;
-		}
-		
-		Refeicao ref = new Refeicao();
-		ref.setNomeRefeicao(ticket.getNomeRefeicao());
-		ref.setValorParcial(0);
-		ref.setValorTotal(0);
-		Refeicao registrada = daoRefeicao.save(ref);
-		System.out.print(registrada);
-		
-		//Long idRef = registrada.getIdRefeicao();
-		//ticket.setIdRefeicao(idRef);
-		
-		//Ticket resultado = daoTicket.save(ticket);
-		
-		//TicketNormal normal = new TicketNormal();
-		//normal.setIdTicket(resultado.getIdTicket());
-		
-		
-		//return ResponseEntity.ok().body(registrada);
-	}
-	*/
-	
 	// Verifica se um cliente já consumiu uma refeição
-	public Ticket ticketConsumido (String cpf, String refeicao, Date data) {
-		
-		List<Ticket> consumidoHOJE = daoTicket.findByCpfClienteAndNomeRefeicaoAndDataConsumo(cpf, refeicao, data);
-		
-		return consumidoHOJE.get(0);
+	public Ticket ticketConsumido (String cpf, String refeicao) {	
+		LocalDate localDate = LocalDate.now();
+        java.sql.Date dataHoje = java.sql.Date.valueOf( localDate );
+		Ticket consumidoHoje = daoTicket.findByCpfClienteAndNomeRefeicaoAndDataConsumo(cpf, refeicao, dataHoje);
+		return consumidoHoje;
 	}
 	
 	public String verificaRefeicao (Date data, Time hora) {
@@ -247,9 +175,89 @@ public class TicketController {
 		
 	}
 	
+	@RequestMapping(value = "/consulta/colaborador/{cpf}", method = RequestMethod.GET, produces="application/json") 
+	public void consultaColaborador (@PathVariable String cpf) {	
+		String buscaColaborador = "SELECT * FROM essgrhu.servidor servidor "
+				+ "JOIN essgrhu.turno turno USING (idturno) "
+				+ "JOIN essgrhu.plantao plantao USING (idplantao) "
+				+ "WHERE servidor.cpfservidor = '" + cpf + "';";
+	}
 	
-	@RequestMapping(value = "/consulta/teste/{cpf}", method = RequestMethod.GET, produces="application/json") 
-	public ResponseEntity<String> teste (@PathVariable String cpf) {
+	@RequestMapping(value = "/consulta/acompanhante/{cpf}", method = RequestMethod.GET, produces="application/json") 
+	public ResponseEntity<String> consultaAcompanhante (@PathVariable String cpf) {	
+		String buscaAcompanhante = "SELECT * FROM essgrhu.acompanhante acompanhante "
+				+ "JOIN essgrhu.paciente paciente USING (idpaciente) "
+				+ "NATURAL JOIN essgrhu.internacao AS internacao "
+				+ "NATURAL JOIN essgrhu.leito  AS leito "
+				+ "NATURAL JOIN essgrhu.unidadeFuncional AS unidadeF "
+				+ "WHERE acompanhante.cpfacompanhante = '" + cpf + "';";
+		
+		String cpfPaciente = "";
+		String nome = "";
+		String sobrenome = "";
+		String refeicao = "";
+		boolean existeAcompanhante = true;
+		boolean ticketConsumido = false;
+		JSONObject record = new JSONObject();
+		
+		try {
+			
+			ResultSet ps = retrieveData(buscaAcompanhante);
+			
+			if (ps.next() == false) {
+				existeAcompanhante = false;
+		      } else {
+
+		        do {
+		        	
+		        	cpfPaciente = ps.getString("cpfpaciente");
+		        	String buscaTodos = "SELECT * FROM essgrhu.acompanhante acompanhante "
+							+ "JOIN essgrhu.paciente paciente USING (idpaciente) "
+							+ "WHERE paciente.cpfpaciente = '" + cpfPaciente + "';";
+		        	Date data = ps.getDate("dataprevisao");
+		        	Time hora = ps.getTime("horaprevisao");
+		        	nome = ps.getString("primeiro_nome");
+		        	sobrenome = ps.getString("sobrenome");
+		        	refeicao = verificaRefeicao(data, hora);
+		        	ResultSet rs = retrieveData(buscaTodos);
+		        	
+		        	while (rs.next()) {
+		        		String acompanhantes = rs.getString("cpfacompanhante");
+		        		Ticket consumido = ticketConsumido(acompanhantes, refeicao);
+		        		if (consumido != null) {
+		        			ticketConsumido = true;
+		        		}
+		        	}       	
+		        } while (ps.next());
+		      }
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		if (!existeAcompanhante) {
+			//System.out.println("Não existe");
+			return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+		} else {
+			//System.out.println(nome);
+			//System.out.println(sobrenome);
+			//System.out.println(refeicao);
+			//System.out.println(ticketConsumido);
+			
+			record.put("Nome", nome);
+			record.put("Sobrenome", sobrenome);
+			record.put("Refeição", refeicao);
+			record.put("Refeição Consumida", ticketConsumido);
+			String result = record.toString();
+		    return new ResponseEntity<String>(result, HttpStatus.OK);
+			
+		}
+		
+	}	
+	
+	@RequestMapping(value = "/consulta/paciente/{cpf}", method = RequestMethod.GET, produces="application/json") 
+	public ResponseEntity<String> consultaPaciente (@PathVariable String cpf) {
 		String buscaInternado = "SELECT * FROM essgrhu.paciente AS paciente "
         		+ "NATURAL JOIN essgrhu.internacao AS internacao "
         		+ "NATURAL JOIN essgrhu.leito  AS leito "
@@ -267,8 +275,8 @@ public class TicketController {
 		boolean temInternacao = true; // Saber se há internação
 		boolean temExame = true; // Saber se há exame
 		boolean direitoLanche = false; // Saber se o paciente com exame tem direito ao Lanche
+		boolean refeicaoConsumida = false;
 		
-		JSONArray entities = new JSONArray();
 		JSONObject record = new JSONObject();
 		
 		try {
@@ -319,6 +327,16 @@ public class TicketController {
 			e.printStackTrace();
 		}
 		
+		//LocalDate datinha = LocalDate.of(2020,3,20);
+		//java.sql.Date sqlDate = java.sql.Date.valueOf( datinha );
+		
+		//Ticket consumido = ticketConsumido("09834523476", "Almoço", sqlDate);
+		Ticket consumido = ticketConsumido(cpf, refeicao);
+		
+		if (consumido != null) {
+			refeicaoConsumida = true;
+		}
+		
 		// Não encontrou o paciente com o cpf fornecido
 		if (temExame == false && temInternacao == false) {
 			return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
@@ -329,6 +347,7 @@ public class TicketController {
 			record.put("Sobrenome", sobrenome);
 			record.put("Refeição", refeicao);
 			record.put("Lanche", direitoLanche);
+			record.put("Refeição Consumida", refeicaoConsumida);
 			String result = record.toString();
 		    return new ResponseEntity<String>(result, HttpStatus.OK);
 		}
